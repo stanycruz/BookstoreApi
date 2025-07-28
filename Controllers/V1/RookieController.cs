f7324c640dc6d50b5737622ba48a3c4b3b70b78c
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-28 17:27:31 -0300

    feat: add rookie controller

---
+++ Controllers/V1/RookieController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreApi.Controllers.V1;
+
+[ApiController]
+[Route("v1/[controller]")]
+public class RookieController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Roles = "rookie")]
+    public IActionResult Get()
+    {
+        var user = User.Identity?.Name ?? "desconhecido";
+        return Ok($"Olá, {user}! Você acessou o endpoint /v1/rookie");
+    }
+}
