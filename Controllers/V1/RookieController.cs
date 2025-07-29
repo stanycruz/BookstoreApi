7251507c66411738989c2af060b8ca33cba07752
Stanislaw Cruz <stanycruz@outlook.com>
2025-07-29 18:48:15 -0300

    refactor: update rookie controller

--- Controllers/V1/RookieController.cs
+++ Controllers/V1/RookieController.cs
@@ -1,3 +1,4 @@
+using BookstoreApi.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,7 +12,7 @@ public class RookieController : ControllerBase
     [Authorize(Roles = "rookie")]
     public IActionResult Get()
     {
-        var user = User.Identity?.Name ?? "desconhecido";
-        return Ok($"Olá, {user}! Você acessou o endpoint /v1/rookie");
+        var user = UserHttpContextHelper.GetCurrentUser(HttpContext);
+        return Ok($"Olá, {user?.Name}! Você está autenticado como '{user?.Role}'");
     }
 }
