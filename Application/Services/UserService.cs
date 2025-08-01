09cb86dc505559840c93284d2c90b532a50073dd
Stanislaw Cruz <stanycruz@outlook.com>
2025-08-01 15:22:24 -0300

    refactor: update mapping for kaycloak

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -15,25 +15,23 @@ public class UserService
 
     public async Task<User> GetOrCreateFromClaimsAsync(ClaimsPrincipal claims)
     {
-        var sub = claims.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-        // TODO Você pode ainda adicionar um fallback,
-        // caso em algum ambiente futuro o sub não seja mapeado automaticamente:
-        // var sub =
-        //     claims.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? claims.FindFirst("sub")?.Value;
-
-        // if (string.IsNullOrWhiteSpace(sub))
-        //     throw new UnauthorizedAccessException("Token sem 'sub' ou 'nameidentifier'.");
+        var sub =
+            claims.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? claims.FindFirst("sub")?.Value;
 
         if (string.IsNullOrWhiteSpace(sub))
-            throw new UnauthorizedAccessException("Token sem 'sub'.");
+            throw new UnauthorizedAccessException("Token sem 'sub' ou 'nameidentifier'.");
 
         var user = _context.Users.FirstOrDefault(u => u.KeycloakId == sub);
         if (user != null)
             return user;
 
         var email = claims.FindFirst(ClaimTypes.Email)?.Value ?? "sem-email@local";
-        var name = claims.FindFirst(ClaimTypes.Name)?.Value ?? "Desconhecido";
+
+        var name =
+            claims.FindFirst(ClaimTypes.Name)?.Value
+            ?? claims.FindFirst("name")?.Value
+            ?? claims.FindFirst("preferred_username")?.Value
+            ?? "Desconhecido";
 
         var role = claims.FindAll(ClaimTypes.Role).FirstOrDefault()?.Value ?? "rookie";
 
